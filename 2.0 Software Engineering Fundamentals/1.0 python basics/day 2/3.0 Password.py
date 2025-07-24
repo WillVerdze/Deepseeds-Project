bb1388b9a91d010ddb4c85ee25d057a4a5a96ecd
WillVerdze <verdzeleinyuyyefon@gmail.com>
2025-07-24 16:15:08 +0100

    Added a password checker

---
+++ 2.0 Software Engineering Fundamentals/1.0 python basics/day 2/3.0 Password.py
@@ -0,0 +1,44 @@
+password =input("Enter your password: ")
+
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+digit = "0123456789"
+has_lowercase = False
+has_uppercase = False
+has_digit = False
+
+for char in password:
+    if char in letters:
+        has_lowercase = True
+        continue
+    if char in upper:
+        has_uppercase = True
+        continue
+    if char in digit:
+        has_digit = True 
+        continue
+if has_lowercase and has_uppercase and has_digit == True:
+    print("Strong password")
+else:
+    print("Try again")
+        
+#Doing the password with flags
+#This method is more efficient.
+l = False
+u = False
+d = False
+
+#password = input("Enter your password: ")
+for letter in password:
+    if letter.islower():
+        l = True
+    elif letter.isupper():
+        u = True
+    elif letter.isdigit():
+        d = True
+    
+if l and u and d:
+    print("Strong password")
+else:
+    print("Weak password")
