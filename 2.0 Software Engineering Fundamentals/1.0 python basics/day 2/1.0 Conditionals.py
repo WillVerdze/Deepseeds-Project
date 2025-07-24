f5554d45a6fd23f31805ec76f73d4161159de853
WillVerdze <verdzeleinyuyyefon@gmail.com>
2025-07-24 10:37:18 +0100

    Added a conditionals file iin day 2 folder. Learning if-else conditional statements.

---
+++ 2.0 Software Engineering Fundamentals/1.0 python basics/day 2/1.0 Conditionals.py
@@ -0,0 +1,61 @@
+# import random
+
+# # 🖼️ Array of ASCII art diagrams
+# ascii_art_diagrams = [
+#     r"""
+#      /\_/\
+#     ( o.o )
+#      > ^ <
+#     """,
+
+#     r"""
+#      _______
+#     |       |
+#     |       O
+#     |      /|\
+#     |      / \
+#     |
+#     =========
+#     """,
+
+#     r"""
+#     _______
+#    /       \
+#   |  (• ◡•)  |
+#    \_______/
+#     """,
+
+#     r"""
+#      __
+#     /  \
+#    |    |
+#    |____|
+#   /      \
+#  /________\
+#     """
+# ]
+
+# # 📝 Array of creative writing prompts
+# story_prompts = [
+#     "Write a story about a robot who wants to be human.",
+#     "Describe a world where plants can talk to people.",
+#     "Create a mystery involving a time-traveling detective.",
+#     "Tell a bedtime story set on a spaceship.",
+#     "Imagine a dragon who’s afraid of fire.",
+#     "Write about a kid who finds a magical notebook.",
+#     "Narrate a short poem about loneliness and the moon.",
+#     "Describe an ancient civilization powered by sound."
+# ]
+
+# age = input("Enter your age: ")
+# if age > 18:
+#     print("You can vote")
+# else:
+#     print("Please wait for  next elections")
+    
+#Exercise: take a person's age, check if they are more than 16, if yes, they can play in the basketball team
+age = int(input("Enter your age: "))
+if age > 16:
+    print("You can play int the basketball team")
+else:
+    print("Wait till you're more than 16")
